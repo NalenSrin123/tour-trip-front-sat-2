301b403c576abf1678072968a24ccc93220bcf9d
novsreyneath <novsreyneath2@gmail.com>
2026-08-29 13:28:55 +0700

    Last Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import ForgotPassword from "./pages/ForgotPassword";
-import ResetPassword from "./pages/ResetPassword";
-
+import ForgotPassword from "./pages/public/ForgotPassword";
+import ResetPassword from "./pages/public/ResetPassword";
+  
 function App() {
   return (
     <Router>
